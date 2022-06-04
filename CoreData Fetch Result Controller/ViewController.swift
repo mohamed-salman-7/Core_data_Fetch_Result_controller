b8f4ded85987215fe295899d548265999624b57c
mohamed salman <mohamedsalman@sparkouttech.com>
2022-06-04 15:44:38 +0530

    Initial Commit

---
+++ CoreData Fetch Result Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData Fetch Result Controller
+//
+//  Created by Mohamed Salman on 04/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
